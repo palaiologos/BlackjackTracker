91d275e46f595514b4ae61197f0f0bba1906c1d6
John Williams <palaiologos1990@gmail.com>
2018-09-07 16:39:17 -0700

    Add drawer view navigation functionality with fragments

--- BlackjackTracker/app/src/main/java/in/williams/john/blackjacktracker/Sessions.java
+++ BlackjackTracker/app/src/main/java/in/williams/john/blackjacktracker/Sessions.java
@@ -1,6 +1,22 @@
 package in.williams.john.blackjacktracker;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class Sessions extends Fragment {
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getActivity().setTitle("Sessions");
+    }
+
+    @Nullable
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.sessions, container, false);
+    }
 }
--- BlackjackTracker/app/src/main/java/in/williams/john/blackjacktracker/Trends.java
+++ BlackjackTracker/app/src/main/java/in/williams/john/blackjacktracker/Trends.java
@@ -7,16 +7,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class Home extends Fragment {
+public class Trends extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        getActivity().setTitle("Home");
+        getActivity().setTitle("Trends");
     }
 
     @Nullable
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home, container, false);
+        return inflater.inflate(R.layout.trends, container, false);
     }
 }
