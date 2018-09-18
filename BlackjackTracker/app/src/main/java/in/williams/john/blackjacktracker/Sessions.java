58e18849a21c7e25eebf609f4a63516ad00b2198
John Williams <palaiologos1990@gmail.com>
2018-09-18 10:28:00 -0700

    Update sessions page

--- BlackjackTracker/app/src/main/java/in/williams/john/blackjacktracker/Sessions.java
+++ BlackjackTracker/app/src/main/java/in/williams/john/blackjacktracker/Sessions.java
@@ -3,6 +3,7 @@ package in.williams.john.blackjacktracker;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -13,7 +14,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.Toast;
+import android.widget.GridLayout.LayoutParams;
 
 public class Sessions extends Fragment {
 
@@ -33,13 +37,16 @@ public class Sessions extends Fragment {
         // Set the title and message.
         builder.setTitle(title);
         builder.setMessage(message);
-        // Shows the alert diaglog message.
+        // Shows the alert dialog message.
         builder.show();
     }
 
 
     @Nullable
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        // Try to populate table rows dynamically based on db rows.
+
         return inflater.inflate(R.layout.sessions, container, false);
     }
 }
