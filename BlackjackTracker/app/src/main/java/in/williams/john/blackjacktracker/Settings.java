8b61aca428922f3eeb3998802070f700971e6a31
John Williams <palaiologos1990@gmail.com>
2019-03-07 13:51:11 -0800

    Add logout button to settings page

--- BlackjackTracker/app/src/main/java/in/williams/john/blackjacktracker/Settings.java
+++ BlackjackTracker/app/src/main/java/in/williams/john/blackjacktracker/Settings.java
@@ -1,18 +1,45 @@
 package in.williams.john.blackjacktracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class Settings extends Fragment {
+
+    Button mButtonLogout;
+    UserAccountManager session;
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         getActivity().setTitle("Settings");
+
+        // Account session.
+        session = new UserAccountManager(getContext());
+
+        // Logout button.
+        mButtonLogout = (Button) getView().findViewById(R.id.button_logout);
+
+        // When you click the logout button, you should logout and go to login screen.
+        mButtonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Clear the shared preferences first.
+                session.logoutUser();
+
+                // Then go to login page.
+                Intent goToLogin = new Intent(getContext(), Login.class);
+                startActivity(goToLogin);
+
+            }
+        });
     }
 
     @Nullable
