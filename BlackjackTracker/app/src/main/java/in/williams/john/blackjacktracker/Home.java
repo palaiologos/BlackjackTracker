62611cd4f9fb9665c037af6b1ac1371f18f09276
John Williams <palaiologos1990@gmail.com>
2019-03-12 13:12:09 -0700

    Built wireframe of home page

--- BlackjackTracker/app/src/main/java/in/williams/john/blackjacktracker/Home.java
+++ BlackjackTracker/app/src/main/java/in/williams/john/blackjacktracker/Home.java
@@ -1,5 +1,7 @@
 package in.williams.john.blackjacktracker;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,17 +11,15 @@ import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.HashMap;
 
 public class Home extends Fragment {
 
-    // An instance to hold the info of whoever is logged in.
-    UserAccountManager session;
-
-    TextView welcome_message;
-    TextView blackjack_news;
+    // Buttons to links.
+    Button blackjack_news;
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
@@ -27,24 +27,22 @@ public class Home extends Fragment {
 
         getActivity().setTitle("Home");
 
-        // Account instance.
-        session = new UserAccountManager(getContext());
-
-        // Get username from database.
-        HashMap<String, String> user = session.getUserDetails();
-        String username = user.get(UserAccountManager.KEY_NAME);
-
-        // Set TextView to contain the text.
-        TextView welcome_message = (TextView) getView().findViewById(R.id.home_page_welcome);
-        welcome_message.setText("Hello, " + username + "!");
-
         // Set blackjack news link.
-        Spanned policy = Html.fromHtml(getString(R.string.bj_news));
-        TextView blackjack_news = (TextView)getView().findViewById(R.id.bj_news);
+        blackjack_news = (Button)getView().findViewById(R.id.bj_news);
 
-        blackjack_news.setText(policy);
-        blackjack_news.setMovementMethod(LinkMovementMethod.getInstance());
 
+        // On click listener for bj news button.
+        blackjack_news.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Link to url of news.
+                String url = "https://assets.bj21.com/newsletters/pdf_files/000/000/028/original/CBJN1805.pdf?1525361174";
+                Intent news = new Intent(Intent.ACTION_VIEW, Uri.parse( url) );
+
+                startActivity(news);
+            }
+        });
 
     }
 
